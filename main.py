e59effa92cad5d1a90fc4646d9c05fa2ec49506f
pupsik <iliaman1993@gmail.com>
2022-05-15 19:05:53 +0300

    initial

---
+++ main.py
@@ -0,0 +1,142 @@
+import sqlite3 as sq
+import os
+from flask import Flask, render_template, url_for, request, flash, session, redirect, abort, g
+from flsite import FDataBase
+
+
+# конфигурация
+DATABASE = '/tmp/flsite.db'
+DEBUG = True
+SECRET_KEY = 'abobus70000'
+
+app = Flask(__name__)
+app.config.from_object(__name__)
+
+app.config.update(dict(DATABASE=os.path.join(app.root_path, 'flsite.db')))
+
+
+def create_db():
+    """Вспомогательная функция для создания БД"""
+    db = connect_db()
+    with app.open_resource('sq_db.sql', mode='r') as f:
+        db.cursor().executescript(f.read())
+        db.commit()
+        db.close()
+
+
+def connect_db():
+    conn = sq.connect(app.config['DATABASE'])
+    conn.row_factory = sq.Row
+    return conn
+
+
+def get_db():
+    '''Соединение с БД если оно еще не установлено'''
+    if not hasattr(g, 'link_db'):
+        g.link_db = connect_db()
+        return g.link_db
+
+
+app.config['SECRET_KEY'] = 'abobys'
+
+# menu = [{"name": "Установка", "url": "install-flask"},
+#         {"name": "Первое приложение", "url": "first-app"},
+#         {"name": "Обратная связь", "url": "contact"},
+#         {"name": "Авторизация", "url": "login"}]
+
+
+@app.route("/")
+@app.route('/index')
+def index():
+    db = get_db()
+    dbase = FDataBase(db)
+    return render_template('index.html', menu=dbase.getMenu(), posts = dbase.getPostsAnonce())
+
+
+@app.route("/about")
+def about():
+    db = get_db()
+    dbase = FDataBase(db)
+    print(url_for('about'))
+    return render_template('about.html', title="О сайте", menu=dbase.getMenu())
+
+
+@app.route("/profile/<username>")
+def profile(username):
+    if 'userLogged' not in session or session['userLogged'] != username:
+        abort(401)
+    return f"Пользователь: {username}"
+
+
+@app.route("/contact", methods=["POST", "GET"])
+def contact():
+    db = get_db()
+    dbase = FDataBase(db)
+    if request.method == "POST":
+        if len(request.form['username']) > 2:
+            flash('Сообщение отправлено', category='success')
+        else:
+            flash('Ошибка отправки', category='error')
+    return render_template('contact.html', title="Обратная связь", menu=dbase.getMenu())
+
+
+@app.route('/add_post', methods=["POST", "GET"])
+def addPost():
+    db = get_db()
+    dbase = FDataBase(db)
+    if request.method == 'POST':
+        if len(request.form['name'])>4 and len(request.form['post'])>10:
+            res = dbase.addPost(request.form['name'], request.form['post'])
+            if not res:
+                flash('Ошибка добавления статьи', category='error')
+            else:
+                flash('Статья добавлена успешно', category='success')
+        else:
+            flash('Ошибка добавления статьи', category='error')
+    return render_template('add_post.html', menu=dbase.getMenu(), title='Добавление статьи')
+
+
+@app.route('/post/<int:id_post>')
+def showPost(id_post):
+    db = get_db()
+    dbase = FDataBase(db)
+    title, post = dbase.getPost(id_post)
+    if not title:
+        abort(404)
+    return render_template('post.html', menu=dbase.getMenu(), title=title, post=post)
+
+
+@app.route("/login", methods=["POST", "GET"])
+def login():
+    db = get_db()
+    dbase = FDataBase(db)
+    print(dbase.getMenu())
+    if 'userLogged' in session:
+        return redirect(url_for('profile', username=session['userLogged']))
+    elif request.method == 'POST' and request.form['username'] == "iliaman" and request.form['psw'] == "123":
+        session['userLogged'] = request.form['username']
+        return redirect(url_for('profile', username=session['userLogged']))
+    return render_template('login.html', title='Авторизация', menu=dbase.getMenu())
+
+
+@app.teardown_appcontext
+def close_db(error):
+    '''Закрываем соединение с БД, если оно было установлено'''
+    if hasattr(g, 'link_db'):
+        g.link_db.close()
+
+
+@app.errorhandler(404)
+def pageNotFound(error):
+    db = get_db()
+    dbase = FDataBase(db)
+    return render_template('page404.html', title='Страница не найдена', menu=dbase.getMenu()), 404
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
+
+# дегустируем url без запуска сервера(искуственно создаем контекст запроса)
+# with app.test_request_context():
+#     print(url_for('about'))
+#     print(url_for('profile', username = "iliaman"))
---
+++ sq_db.sql
@@ -0,0 +1,12 @@
+CREATE TABLE IF NOT EXISTS mainmenu (
+    id integer PRIMARY KEY AUTOINCREMENT,
+    title text NOT NULL,
+    url text NOT NULL
+);
+
+create table if not exists posts (
+    id integer primary key autoincrement,
+    title text not null,
+    text text not null,
+    time integer not null
+)
